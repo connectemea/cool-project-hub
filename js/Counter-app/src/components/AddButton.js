4f5438667a90aa7d7fd97460c650fbcf38e84c5a
aseelkp <aseelaroor10@gmail.com>
2021-11-01 00:35:05 +0530

    nthg

--- js/Counter-app/src/components/AddButton.js
+++
@@ -1,7 +0,0 @@
-import Styles from './style.module.css'
-
-const AddButton = (props) =>{
-    const {onClickHandler,todo} = props;
-    return <button className={todo? Styles.addButton:Styles.addButtonMuted} onClick={todo? onClickHandler:null}>Add</button>
-}
-export default AddButton;
